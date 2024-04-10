05348fc248d890ed4c6d77d6d6c89f3b2c2acd93
KaraboWits <160496062+KaraboWits@users.noreply.github.com>
2024-04-10 12:23:50 +0000

    creating api functions

---
+++ src/apifunctions.js
@@ -0,0 +1,70 @@
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    // Set the response headers
+    context.res = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    // Initialize the cars data (you can load this from a JSON file as you did in Express.js)
+    const cars = [];
+
+    // Handle HTTP GET request to retrieve all cars
+    if (req.method === 'GET') {
+        context.res.body = cars;
+    }
+
+    // Handle HTTP GET request to retrieve a car by ID
+    if (req.method === 'GET' && req.params.id) {
+        const id = req.params.id;
+        const car = cars.find(car => car.id === id);
+        if (car) {
+            context.res.body = car;
+        } else {
+            context.res = {
+                status: 404,
+                body: 'Car not found'
+            };
+        }
+    }
+
+    // Handle HTTP POST request to add a new car
+    if (req.method === 'POST') {
+        const newCar = req.body;
+        cars.push(newCar);
+        context.res.body = newCar;
+    }
+
+    // Handle HTTP PUT request to update a car by ID
+    if (req.method === 'PUT' && req.params.id) {
+        const id = req.params.id;
+        const updatedCar = req.body;
+        const index = cars.findIndex(car => car.id === id);
+        if (index !== -1) {
+            cars[index] = updatedCar;
+            context.res.body = updatedCar;
+        } else {
+            context.res = {
+                status: 404,
+                body: 'Car not found'
+            };
+        }
+    }
+
+    // Handle HTTP DELETE request to delete a car by ID
+    if (req.method === 'DELETE' && req.params.id) {
+        const id = req.params.id;
+        const index = cars.findIndex(car => car.id === id);
+        if (index !== -1) {
+            cars.splice(index, 1);
+            context.res.body = { message: `Car with id ${id} deleted` };
+        } else {
+            context.res = {
+                status: 404,
+                body: 'Car not found'
+            };
+        }
+    }
+};
